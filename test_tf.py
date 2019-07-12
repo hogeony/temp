677a68ee6be0ab37209d3aca626b43035c667a2f
Hogeon Seo <hogeony@gmail.com>
2019-07-12 15:00:47 +0900

    Update test_tf.py

--- test_tf.py
+++ test_tf.py
@@ -21,14 +21,13 @@ class Model(Model):
     def call(self, inputs):
         return self.layer(inputs)
 
-CLASSIFIER_DIM = 10
+(x_train, y_train),(x_test, y_test) = datasets.mnist.load_data()
+x_train, x_test = x_train.astype('float32') / 255.0, x_test.astype('float32') / 255.0
+x_train, x_test = x_train.reshape((60000, 28, 28, 1)), x_test.reshape((10000, 28, 28, 1))
 
 c = Model()
 optimizer = optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
 c.compile(optimizer='adam', loss=losses.SparseCategoricalCrossentropy(), metrics=[metrics.SparseCategoricalAccuracy()])
-(x_train, y_train),(x_test, y_test) = datasets.mnist.load_data()
-x_train, x_test = x_train.astype('float32') / 255.0, x_test.astype('float32') / 255.0
-x_train, x_test = x_train.reshape((60000, 28, 28, 1)), x_test.reshape((10000, 28, 28, 1))
 
 time_start = time.time()
 c.fit(x_train, y_train, epochs=3, batch_size=100, shuffle=True)
